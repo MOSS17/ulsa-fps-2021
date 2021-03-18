c10627c29bfbbd46c1d75722e7452ee037b45b7d
MOSS17 <42181277+MOSS17@users.noreply.github.com>
2021-03-18 14:00:35 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    PlayerInputs playerInputs;
+
+    [SerializeField, Range(0.1f, 10f)] float moveSpeed = 5f;
+
+    [SerializeField] Transform camTrs;
+    [SerializeField, Range(0.1f, 180f)] float camRotSpeed = 90f;
+    [SerializeField] float xClamp = 85f;
+    float xRotation = 0f;
+    float camRotationAmounthY;
+
+    [SerializeField, Range(0.1f, 15f)] float jumpForce = 5f;
+    [SerializeField, Range(0.1f, 10f)] float rayLength = 1f;
+    [SerializeField] Color rayColor = Color.magenta;
+    [SerializeField] LayerMask detectionLayer;
+    [SerializeField]Vector3 rayPosition;
+
+    [SerializeField, Range(1f, 5f)] float augmentedFactor = 1f;
+    float augmentedSpeed = 1f;
+    float baseSpeed = 1f;
+
+    [SerializeField] List<Weapon> weapons;
+    int weaponIndex = 0;
+
+    void Awake() {
+        rb ??= GetComponent<Rigidbody>();
+        playerInputs ??= new PlayerInputs();
+    }
+
+    void OnEnable() {
+        playerInputs?.Enable();
+    }
+
+    void OnDisable() {
+        playerInputs?.Disable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerInputs.Gameplay.Jump.performed += _ => Jump();
+        playerInputs.Gameplay.Run.performed += _ => augmentedSpeed = augmentedFactor;
+        playerInputs.Gameplay.Run.canceled += _ => augmentedSpeed = baseSpeed;
+        playerInputs.Gameplay.Shoot.performed += _ => CurrentWeapon.Shoot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        camRotationAmounthY += CamAxis.x * camRotSpeed * Time.deltaTime;
+        rb.rotation = Quaternion.Euler(rb.rotation.x, camRotationAmounthY, rb.rotation.z);
+        rb.position += Forward *moveSpeed * augmentedSpeed * Time.deltaTime;
+
+        //restriccion de la camara en Y
+        xRotation -= CamAxis.y * camRotSpeed * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
+        Vector3 targetRotation = transform.eulerAngles;
+        targetRotation.x = xRotation;
+        camTrs.eulerAngles = targetRotation;
+
+        if(WheelAxisYClamped != 0f)
+        {
+            CurrentWeapon.Active(false);
+            //inicio cambio de arma
+            /*if(WheelAxisYClampInt + weaponIndex >= 0 && WheelAxisYClampInt + weaponIndex < weapons.Count)
+            {
+                weaponIndex += WheelAxisYClampInt;
+            }
+            else if()*/
+           /* weaponIndex += WheelAxisYClampInt + weaponIndex >= 0 && 
+            WheelAxisYClampInt + weaponIndex < weapons.Count ? 
+            WheelAxisYClampInt + weaponIndex == weapons.Count ?
+            0:  WheelAxisYClampInt : weapons.Count - 1;
+            Debug.Log(weaponIndex);*/
+            if(WheelAxisYClampInt + weaponIndex >= 0)
+            {
+                if(WheelAxisYClampInt + weaponIndex < weapons.Count)
+                {
+                    weaponIndex += WheelAxisYClampInt;
+                }
+                else
+                {
+                    if(WheelAxisYClampInt + weaponIndex >= weapons.Count)
+                    {
+                        weaponIndex = 0;
+                    }
+                }
+            }
+            else
+            {
+                weaponIndex = weapons.Count - 1;
+            }
+            //fin cambio de arma
+            CurrentWeapon.Active(true);
+
+            Debug.Log(weaponIndex);
+        }
+    }
+
+    void Jump()
+    {
+        if(!Grounding) return;
+        rb.AddForce(JumpDirection, ForceMode.Impulse);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(RelativeRayPosition, -transform.up * rayLength);
+    }
+
+    bool Grounding => Physics.Raycast(RelativeRayPosition, -transform.up, rayLength, detectionLayer);
+
+    Vector2 Axis => playerInputs.Gameplay.Movement.ReadValue<Vector2>();
+
+    float CamAxisX => playerInputs.Gameplay.CamAxisX.ReadValue<float>();
+    float CamAxisY => playerInputs.Gameplay.CamAxisY.ReadValue<float>();
+
+    Vector2 CamAxis => new Vector2(CamAxisX, CamAxisY);
+    Vector3 MovementAxis => new Vector3(Axis.x, 0f, Axis.y);
+    Vector3 Forward => rb.rotation * MovementAxis;
+    Vector3 JumpDirection => Vector3.up * jumpForce;
+    Vector3 RelativeRayPosition => rayPosition + transform.position;
+
+    int WheelAxisYClampInt => (int)Mathf.Ceil(WheelAxisYClamped);
+
+    float WheelAxisYClamped => Mathf.Clamp(WheelAxisY, -1, 1);
+
+    float WheelAxisY => playerInputs.Gameplay.WeaponChange.ReadValue<float>();
+
+    Weapon CurrentWeapon => weapons[weaponIndex];
+
+    //Weapon CurrentWeapon => weapons[0];
+    
+
+}
